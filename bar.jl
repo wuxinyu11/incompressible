752cfa56a61ada5501221920a6685471a7a82e66
Wu Junchao <jmwjc@163.com>
2022-10-20 00:23:48 +0800

    20221019@wujc 还没改完

--- bar.jl
+++ bar.jl
@@ -29,12 +29,12 @@ set𝝭!(elements2["Ω"])
 set∇̃𝝭!(elements2["Ω̃"],elements2["Ω"])
 set∇𝝭!(elements2["Γᵍ"])
 
-prescribe!(elements1["Ω"],:b=>(x,y,z)->0.0)
+prescribe!(elements1["Ω"],:b=>(x,y,z)->1.0)
 prescribe!(elements1["Γᵍ"],:g=>(x,y,z)->x)
 prescribe!([elements1["Γᵍ"][1]],:n₁=>(x,y,z)->-1.0)
 prescribe!(elements1["Γᵍ"][2],:n₁=>(x,y,z)->1.0)
 
-prescribe!(elements2["Ω"],:b=>(x,y,z)->0.0)
+prescribe!(elements2["Ω"],:b=>(x,y,z)->1.0)
 prescribe!(elements2["Γᵍ"],:g=>(x,y,z)->x)
 prescribe!([elements2["Γᵍ"][1]],:n₁=>(x,y,z)->-1.0)
 prescribe!(elements2["Γᵍ"][2],:n₁=>(x,y,z)->1.0)
@@ -51,20 +51,20 @@ k1 = zeros(nₚ₁,nₚ₁)
 f1 = zeros(nₚ₁)
 k2 = zeros(nₚ₂,nₚ₂)
 f2= zeros(nₚ₂)
-ops[1](elements1["Ω̃"],k1)
-ops[2](elements1["Ω"],f1)
+ops[1](elements1["Ω̃"][2],k1)
+# ops[2](elements1["Ω"],f1)
 # ops[3](elements["Γᵗ"],f)
-ops[4](elements1["Γᵍ"],k1,f1)
-ops[5](elements1["Γᵍ"],k1,f1)
+# ops[4](elements1["Γᵍ"],k1,f1)
+# ops[5](elements1["Γᵍ"],k1,f1)
 
-ops[1](elements2["Ω̃"],k2)
-ops[2](elements2["Ω"],f2)
+ops[1](elements2["Ω̃"][2],k2)
+# ops[2](elements2["Ω"],f2)
 # ops[3](elements["Γᵗ"],f)
-ops[4](elements2["Γᵍ"],k2,f2)
-ops[5](elements2["Γᵍ"],k2,f2)
+# ops[4](elements2["Γᵍ"],k2,f2)
+# ops[5](elements2["Γᵍ"],k2,f2)
 
-d1 = k1\f1
-d2 = k2\f2
+# d1 = k1\f1
+# d2 = k2\f2
 F=f1-f2
 K=k1-k2
 D=d1-d2
