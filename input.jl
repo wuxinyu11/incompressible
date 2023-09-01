381c3e3522f8395bf4b0e6288cdf4014df7fa14b
xuytxuyt <xyt5517@163.com>
2023-09-01 17:22:50 +0800

    20230901

--- input.jl
+++ input.jl
@@ -205,6 +205,51 @@ function import_tri3(filename::String)
     return elements,nodes
 end
 
+function import_tri3_2(filename1::String,filename2::String)
+    elms,~ = ApproxOperator.importmsh(filename1)
+    elms_p,~ = ApproxOperator.importmsh(filename2)
+    nₚ = length(elms["Ω"][1].x)
+
+    sp = ApproxOperator.RegularGrid(x,y,z,n=1,γ=2)
+    parameters = (:Linear2D,:□,:CubicSpline)
+    n𝒑 = 21
+    scheme = ApproxOperator.quadraturerule(s)
+    
+    d = zeros(nₚ)
+    elements = Dict{String,Vector{ApproxOperator.AbstractElement}}()
+
+    f_Ω = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13)
+    f_Ωᵖ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Tri3},:TriGI13)
+    f_Γᵍ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(ReproducingKernel{parameters...,:Seg2},:SegGI2)
+
+    elements["Ω"] = f_Ω(elms["Ω"])
+    elements["Ωᵖ"] = f_Ωᵖ(elms_p["Ω"])
+    elements["Γᵍ"] = f_Γᵍ(elms["Γᵍ"])
+    push!(f_Ω,
+        :d=>(:𝐼,d),
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+    )
+    push!(f_Ωᵖ,
+        :d=>(:𝐼,d),
+        :𝝭=>:𝑠,
+        :∂𝝭∂x=>:𝑠,
+        :∂𝝭∂y=>:𝑠,
+    )
+    push!(f_Γᵍ,
+        :𝝭=>:𝑠,
+    )
+    if haskey(elms,"Γᵗ")
+        f_Γᵗ = ApproxOperator.Field{(:𝐼,),1,(:𝑔,:𝐺,:𝐶,:𝑠),4}(Element{:Seg2},:SegGI2)
+        elements["Γᵗ"] = f_Γᵗ(elms["Γᵗ"])
+        push!(f_Γᵗ,
+            :𝝭=>:𝑠,
+        )
+    end
+    return elements, d, elms_p
+end
+
 function import_quad(filename::String)
     elms,nds = ApproxOperator.importmsh(filename)
     nₚ = length(nds)
